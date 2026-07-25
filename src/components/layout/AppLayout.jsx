a25b6f8900bb9c3ea8a7bd066252ea34d9425891
Faber Solano <benditocyc@gmail.com>
2026-07-24 19:14:13 -0500

    Sprint UX 1.0 - Optimización Layout Tablet

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -1,18 +1,18 @@
 export default function Layout({ header, sidebar, children }) {
   return (
-    <div className="min-h-screen bg-slate-100">
+    <div className="min-h-screen bg-slate-100 flex flex-col">
 
       <header>
         {header}
       </header>
 
-      <div className="flex h-[calc(100vh-64px)]">
+      <div className="flex flex-1 overflow-hidden">
 
         <aside className="hidden lg:block w-72 border-r bg-white overflow-y-auto">
           {sidebar}
         </aside>
 
-        <main className="flex-1 overflow-y-auto p-4">
+        <main className="flex-1 overflow-y-auto px-2 py-3 lg:px-4 lg:py-4">
           {children}
         </main>
 
